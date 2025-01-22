6ffe0a2204fa4223c0c18bf2924be2434f9e6b1e
Gabriel Assis <cardimg06@gmail.com>
2025-01-22 18:29:20 -0300

    feat: add jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,15 @@ import { TaskModule } from './task/task.module';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TaskModule, UsersModule],
+  imports: [ConfigModule.forRoot({isGlobal: true}), 
+    TaskModule, 
+    UsersModule, 
+    AuthModule
+  ],
   controllers: [AppController, UsersController],
   providers: [AppService, UsersService],
 })
